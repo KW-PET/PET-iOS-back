2b029005009ab6d87808668757239d7530c11878
dahyuun <mdahyun1025@naver.com>
2022-10-17 17:24:56 +0900

    초기 틀 설정, 카카오 로그인, 로그아웃(아마도!)

--- src/main/java/com/kw/pet/controller/login/OAuthController.java
+++ src/main/java/com/kw/pet/controller/login/OAuthController.java
@@ -63,11 +63,6 @@ public class OAuthController {
 //            userRepository.save(user);
 //        }
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/main
         return ResponseEntity.ok("사용자 정보 받아옴");
 
         //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
@@ -87,11 +82,6 @@ public class OAuthController {
         return ResponseEntity.ok("로그아웃");
     }
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/main
     @GetMapping("/getfriends")
     public Object getFriends(@PathParam(value = "access_token") String access_token){
 //        oauthService.getFriends(access_token);
@@ -102,10 +92,6 @@ public class OAuthController {
         ResponseEntity<String> response;
         headers.add("Authorization", "Bearer " + access_token);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
 //        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
         String redirect_uri="https://kapi.kakao.com/v1/api/talk/friends";
 
