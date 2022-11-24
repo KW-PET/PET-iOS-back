9aac9b47f85af30c1d328daece3a26ec12840ea8
dahyuun <mdahyun1025@naver.com>
2022-11-24 14:23:38 +0900

    access_Token 이용하여 로그인

--- src/main/java/com/kw/pet/config/SecurityProperties.java
+++ src/main/java/com/kw/pet/config/SecurityProperties.java
@@ -3,5 +3,7 @@ package com.kw.pet.config;
 public interface SecurityProperties {
     String client_id="79e7aeaf2728c5a875d456ac828fba76"; //restapi key
     String redirect_uri = "http://localhost:8080/kakao/login";
+    //String redirect_uri = "http://49.50.164.40:8080/kakao/login";
+
 }
 
